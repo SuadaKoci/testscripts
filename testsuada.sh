76c1489bdb202414da5f6cd8072862b31d97f95b
Suada Koci <Suada.koci@fshnstudent.info>
2021-08-12 19:59:46 +0200

    my first script

--- testsuada.sh
+++ testsuada.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Une jam Suada" 
+if cat /etc/passwd | grep sysop
+then
+	echo "User is on the system"
+	cat /etc/passwd | grep sysop
+	cat /etc/group | grep sysop
+else 
+	echo "User is not in the system"
+	useradd -m sysop 
+	echo "user added"
+	cat /etc/passwd | grep sysop
+       	cat /etc/group | grep sysop
+fi       	
+
